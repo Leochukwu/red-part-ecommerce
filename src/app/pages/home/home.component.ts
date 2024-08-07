40c708c4ad727648e2866ea7db1756c4864a4e13
Leochukwu <leocchukwu@gmail.com>
2024-08-07 12:20:54 +0100

    Owl Carousel slider added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,15 +2,40 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,  HttpClientModule, RouterModule],
+  imports: [CommonModule,  HttpClientModule, RouterModule, CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent  implements OnInit{
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['Prev', 'Next'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
   
   Products: any = []
   item: any;
